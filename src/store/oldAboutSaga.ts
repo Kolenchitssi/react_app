7887e2c3e572359dfc60feb0fe594ddf623b6b7a
KolenchitsSI <sergei.kolenchits@hiqo-solutions.com>
2021-10-19 17:07:36 +0300

    fix: edit errors eslint

--- src/store/oldAboutSaga.ts
+++ src/store/oldAboutSaga.ts
@@ -1,4 +1,4 @@
-import type { RootState, AppDispatch, FormType, BaseAction } from "./models";
+import type { RootState, AppDispatch, FormType, BaseAction } from './models';
 //эффекты -вспомогательные функции которые создают простые объекты котрые содержат инструкции
 //эти инструкции выполняются самой reduxSagaMiddleware
 
@@ -24,7 +24,7 @@ import {
   spawn,
   join,
   select,
-} from "redux-saga/effects";
+} from 'redux-saga/effects';
 import {
   ADD_ARTICLE,
   EDIT_ARTICLE,
@@ -33,24 +33,24 @@ import {
   REMOVE_ARTICLE,
   REMOVE_ARTICLE_LOCALSTORAGE,
   SET_ARTICLE_TO_STATE,
-} from "../routes/Main/modules/store/action";
-import { articlesList } from "../routes/Main/components/Article/articlesList";
-import { ActionType } from "@redux-saga/types";
+} from '../routes/Main/modules/store/action';
+import { articlesList } from '../routes/Main/components/Article/articlesList';
+import { ActionType } from '@redux-saga/types';
 import {
   watchClickRemoveSaga,
   workerRemoveArticles,
-} from "../routes/Main/modules/sagas/removeArticleFromLocalStorage";
-import { watchClickAddSaga } from "../routes/Main/modules/sagas/addArticleToLocalStorage";
-import { watchGetArticles } from "../routes/Main/modules/sagas/getArticleFromLocalStorage";
-import { watchClickEditSaga } from "../routes/Main/modules/sagas/EditArticleInLocalStorage";
+} from '../routes/Main/modules/sagas/removeArticleFromLocalStorage';
+import { watchClickAddSaga } from '../routes/Main/modules/sagas/addArticleToLocalStorage';
+import { watchGetArticles } from '../routes/Main/modules/sagas/getArticleFromLocalStorage';
+import { watchClickEditSaga } from '../routes/Main/modules/sagas/EditArticleInLocalStorage';
 
 // const refreshLocalStorage = (store: RootState) =>
 //   localStorage.setItem("articles", JSON.stringify(store.reducerStarter));
 
 // const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
-//worker saga которая будет запускатся в зависимости от какого-то экшена
-//выполняет бизнес логику(запрос/таймаут/запись в кэш и т.д.)
+// worker saga которая будет запускатся в зависимости от какого-то экшена
+// выполняет бизнес логику(запрос/таймаут/запись в кэш и т.д.)
 
 // export function* workerSaga(action: BaseAction<FormType>) {
 //   yield delay(500);
@@ -73,7 +73,7 @@ import { watchClickEditSaga } from "../routes/Main/modules/sagas/EditArticleInLo
 //   return JSON.parse(localStorageList);
 // };
 
-//получение списка артиклей из localstorage
+// получение списка артиклей из localstorage
 // export function* workerGetArticles() {
 //   yield delay(2000);
 //   const newStore: FormType[] = getLocalStorage();
@@ -81,7 +81,7 @@ import { watchClickEditSaga } from "../routes/Main/modules/sagas/EditArticleInLo
 //   yield put({ type: SET_ARTICLE_TO_STATE, payload: newStore });
 // }
 
-//Удаление  артикля из localstorage
+// Удаление  артикля из localstorage
 // export function* workerRemoveArticles() {
 //   yield delay(1000);
 //   const newStore: FormType[] = getLocalStorage();
@@ -91,8 +91,8 @@ import { watchClickEditSaga } from "../routes/Main/modules/sagas/EditArticleInLo
 //   yield put({ type: REMOVE_ARTICLE, payload: "id" });
 // }
 
-//saga watcher  следят за dispatch'ем action  в приложении и когда происходит
-//какойто экшен они выполняют какое-то действие запускают worker
+// saga watcher  следят за dispatch'ем action  в приложении и когда происходит
+// какойто экшен они выполняют какое-то действие запускают worker
 
 // export function* watchClickEditSaga() {
 //   yield takeEvery(EDIT_ARTICLE, workerSaga);
@@ -102,7 +102,7 @@ import { watchClickEditSaga } from "../routes/Main/modules/sagas/EditArticleInLo
 //   const action: BaseAction<FormType> = yield takeEvery(ADD_ARTICLE, workerSaga);
 // }
 
-//cоздает самый верхний процесс запускает watcher-ы
+// cоздает самый верхний процесс запускает watcher-ы
 
 export function* rootSaga() {
   // yield watchClickSaga();
@@ -112,5 +112,4 @@ export function* rootSaga() {
   yield fork(watchClickEditSaga);
   yield fork(watchClickAddSaga);
   yield fork(watchClickRemoveSaga);
-
 }
